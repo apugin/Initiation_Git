7861c6a45c50d63f582e9ad6d776054b0a025b71
Alex <alexandre.pugin@laposte.net>
2020-09-19 18:42:59 +0200

    Stp marche

--- data.py
+++ data.py
@@ -1,20 +1,82 @@
 from skimage import io
 from skimage import transform
-import matplotlib.pyplot as plt
+from skimage import metrics
+import os
+import tensorflow as tf
 import numpy as np
 
-
-def data_set(**kwargs):  # fonctoin à utiliser dans test et train
+def false_data_set(**kwargs):  # fonctoin à utiliser dans test et train
     return np.random.random((16,32,32,3))
 
 
-def modif():  # instructions pour modif
-    image = io.imread("C:/Users/lucas/Desktop/Chris_AI/Projet/data/Kvasir-SEG/images/cju0qkwl35piu0993l0dewei2.jpg")
-    resized_image = transform.resize(image, (32, 32))
-    io.imshow(resized_image)
-    plt.show()
+def data_set_32(pictures_tab):  # instructions pour modif, en entrée un tableau d'images, en sortie le tableau d'images modifiées
+    n = pictures_tab.shape[0]
+    resized_pictures_tab = tf.zeros([n,1,32,32,3])
+    for k in range(n):
+        resized_picture = modif_picture_32(pictures_tab[k,1])
+        resized_pictures_tab[n,0] = resized_picture
+    return resized_pictures_tab
+
+def modif_picture_32(picture):  #instructions pour modifier les images une par une
+    resized_picture = transform.resize(picture, (32, 32))
+    return resized_picture
 
 
+def data_set_64(pictures_tab):  # instructions pour modif, en entrée un tableau d'images, en sortie le tableau d'images modifiées
+    n = pictures_tab.shape[0]
+    resized_pictures_tab = tf.zeros([n,1,64,64,3])
+    for k in range(n):
+        resized_picture = modif_picture_64(pictures_tab[k,1])
+        resized_pictures_tab[n,0] = resized_picture
+    return resized_pictures_tab
+
+def modif_picture_64(picture):  #instructions pour modifier les images une par une
+    resized_picture = transform.resize(picture, (64, 64))
+    return resized_picture
 
 
+dirpath = "C:/Users/lucas/Desktop/Chris_AI/Projet/data/Kvasir-SEG/images"
+
+
+def create_pictures_tab(dirpath):
+    list_files = os.listdir(dirpath)
+    n = len(list_files)
+    all_picture_tab = tf.zeros([n,1,622,529,3])
+    for k in range(n):
+        final_path = dirpath + "/" + list_files[k]
+        picture = io.imshow(final_path)
+        all_picture_tab[k,0] = picture
+    return all_picture_tab
+
+data_set = create_pictures_tab(dirpath)
+
+data_set__32 = data_set_32(data_set)
+
+data_set__64 = data_set_64(data_set)
+
+data_set_32_64 = data_set_64(data_set__64)
+
+def variance(false_data_set,true_data_set):
+    Metric = []
+    n = len(false_data_set)
+    for k in range(n):
+        m = metrics.peak_signal_noise_ratio(true_data_set[n], false_data_set[n], data_range=None)
+        Metric.append(m)
+    mean = sum(Metric)/n
+    variance_2 = 0
+    for k in range(n):
+        variance_2 = variance_2 + (Metric[n] - mean)/n
+    variance = variance_2**(1/2)
+    return (variance)
+
+def len_test_data_set(false_data_set,true_data_set,alpha):  # pour un alpha donne, on veut une précision d'au moins 1-alpha
+    var = variance(false_data_set,true_data_set)
+    n_min = var/(alpha**3)
+    return n_min
+
+#len_test = len_test_data_set(data_set__64, data_set_32_64,0.05)
+
+#print(len_test)
+
+print(type(data_set))
 
