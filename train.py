45d26f1b2e35c0014e1623612c6942fdb5fbe214
BuildTools <jules.salzinger@ec-m.fr>
2020-09-19 14:40:54 +0200

    faire en sorte que ca marche en fait

--- train.py
+++ train.py
@@ -2,5 +2,8 @@ import tensorflow as tf
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout, Activation
 import numpy as np
-def train(Model,data,labels,batch_size=64,epochs=5):
+def train(Model,data,label,batch_size=64,epochs=5):
+    label = label.reshape((len(label), np.prod(label.shape[1:])))
+    data = data.reshape((len(data), np.prod(data.shape[1:])))
     Model.fit(data,labels, batch_size=64, epochs =5)
+    
