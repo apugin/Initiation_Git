8dcffe2083333fb86a324d8650bf7989bf9138fd
BuildTools <jules.salzinger@ec-m.fr>
2020-09-19 15:30:32 +0200

    modele convolutif

--- model.py
+++ model.py
@@ -1,13 +1,11 @@
 import tensorflow as tf
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout, Activation
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout, Activation, UpSampling
 import numpy as np
 def build_network(resolution=(32,32,3),new_resolution =(60,60,3)):
     #Input_shape = (resolution[0]*resolution[1],)
     Model = Sequential()
-    Model.add(Dense(new_resolution[0]*new_resolution[1]*new_resolution[2], activation ='relu'))
-    Model.compile(
-    optimizer='adam',
-    loss='binary_crossentropy',
-    metrics=['accuracy'])
+    Model.add(UpSampling2D((2,2), input_shape = (32,32,3,)))
+    Model.add(Conv2D(filters= 32, kernel_size =(5,5) , padding = 'same'))
+    Model.add(Conv2D(filters= 3, kernel_size =(4,4) , padding = 'same'))
     return(Model)
--- train.py
+++ train.py
@@ -2,8 +2,12 @@ import tensorflow as tf
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout, Activation
 import numpy as np
-def train(Model,data,label,batch_size=64,epochs=5):
-    label = label.reshape((len(label), np.prod(label.shape[1:])))
-    data = data.reshape((len(data), np.prod(data.shape[1:])))
-    Model.fit(data,labels, batch_size=64, epochs =5)
+import data
+def train(Model,label,batchsize=64,epochs=5):
+    data = data_set()
+    Model.compile(
+    optimizer='adam',
+    loss='binary_crossentropy',
+    metrics=['accuracy'])
+    Model.fit(data,labels, batch_size=batchsize, epochs =epochs)
     
