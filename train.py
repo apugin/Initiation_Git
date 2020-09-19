38f7442b22be84c8a773e623a2aa806c8c60fea1
Lucas <lucas.chardin@centrale-marseille.fr>
2020-09-19 14:00:50 +0200

    création d'une fonction (sensée) entraîner le modèle

--- train.py
+++ train.py
@@ -1,1 +1,6 @@
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout, Activation
 import numpy as np
+def train(Model,data,labels,batch_size=64,epochs=5):
+    Model.fit(data,labels, batch_size=64, epochs =5)
