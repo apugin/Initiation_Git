056dc477bfd528fd8b608316836e613934cd3902
Alex <alexandre.pugin@laposte.net>
2020-09-19 16:48:11 +0200

    test est commencé et utilities contient mse, psnr et ssim

--- test.py
+++ test.py
@@ -1,1 +1,18 @@
-import numpy as np
+from data import data_set
+from model import build_network
+from utilities import *
+
+
+def test(epoch, ckpt_dir):
+    network = build_network()
+    network.load_weights(ckpt_dir)  # Comment récupérer les poids d'une epoch donnée ?
+    data_test = data_set('test')
+    inputs = data_test[0]
+    label = data_test[1]
+    predicted = network.predict(inputs)
+    PSNR = psnr(label, predicted)
+    SSIM = ssim(label, predicted)
+    print('PSNR :')
+    print(PSNR)
+    print('SSIM :')
+    print(SSIM)
--- utilities.py
+++ utilities.py
@@ -1,1 +1,13 @@
-import numpy as np
+import skimage
+
+
+def psnr(true_image,up_sampled_image):
+    return skimage.metrics.peak_signal_noise_ratio(true_image, up_sampled_image)
+
+
+def mse(true_image,up_sampled_image):
+    return skimage.metrics.mean_squared_error(true_image, up_sampled_image)
+
+
+def ssim(true_image,up_sampled_image):
+    return skimage.metrics.structural_similarity(true_image,up_sampled_image)
